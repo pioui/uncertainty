fef7ce7507ff03934b58e02e6142f29a0aef92ed
pioui <piyilozou@gmail.com>
2022-06-08 22:57:47 +0200

    classifiaction packaging

--- scripts/bcss_config.py
+++ scripts/bcss_config.py
@@ -1,18 +1,23 @@
 import logging
-import numpy as np
+import os
 
 from uncertainty.datasets import bcss_Dataset
-images_dir = "/home/pigi/repos/BCSS/images/"
-outputs_dir = "/home/pigi/repos/BCSS/outputs/"
+
+images_dir = "outputs/images/"
+outputs_dir = "outputs/"
 data_dir = "/home/pigi/repos/BCSS/"
 
-samples_per_class = 10
-project_name = "BCSS_Uncertainty"
+if not os.path.exists(outputs_dir):
+    os.makedirs(outputs_dir)
 
-logging.basicConfig(filename = f'{outputs_dir}{project_name}_logs.log')
+if not os.path.exists(images_dir):
+    os.makedirs(images_dir)
 
-DATASET = bcss_Dataset(
+samples_per_class = 2000
+dataset = bcss_Dataset(
         data_dir = data_dir,
         samples_per_class=samples_per_class
     )
 
+project_name = "BCSS_Uncertainty"
+logging.basicConfig(filename = f'{outputs_dir}{project_name}_logs.log')
--- scripts/classification.py
+++ scripts/classification.py
@@ -7,91 +7,75 @@ import numpy as np
 from matplotlib import colors
 from sklearn.metrics import confusion_matrix
 
-from scripts.bcss_config import data_dir, outputs_dir, images_dir
-
-from bcss_dataset import bcss_Dataset
-from read_data import labels, N_LABELS
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "--dataset", "-d",
     help="name of dataset to use (trento, bcss)",
+    default="bcss"
     )
 
 args = parser.parse_args()
-dataset = args.dataset
-
-if dataset == 'bcss':
+dataset = args.dataset    
     
+if dataset == 'bcss':
+    from bcss_config import (
+    data_dir, 
+    outputs_dir, 
+    images_dir,
+    dataset
+)
 
-
-X_train,y_train = DATASET.train_dataset.tensors # 819
+X_train,y_train = dataset.train_dataset 
 print(X_train.shape, y_train.shape, np.unique(y_train))
-X_test,y_test = DATASET.test_dataset.tensors # 15107
+X_test,y_test = dataset.test_dataset 
 print(X_test.shape, y_test.shape, np.unique(y_test))
-X,y = DATASET.full_dataset.tensors # 15107
+X,y = dataset.full_dataset 
 print(X.shape, y.shape, np.unique(y))
 
 # ----- SVM -----#
 
-clf_svm = SVC(C=1, kernel="rbf", verbose=True)
-clf_svm.fit(X_train.numpy(), y_train.numpy(), verbose=True)
-svm_accuracy = clf_svm.score(X_test.numpy(), y_test.numpy())
-print(svm_accuracy)
+clf_svm = SVC(
+    C=1, 
+    kernel="rbf", 
+    verbose=True
+    )
+clf_svm.fit(X_train, y_train)
+svm_accuracy = clf_svm.score(X_test, y_test)
+print(f"SVM Accuracy : {svm_accuracy}")
 
-y_pred = clf_svm.predict(X_test.numpy())
-m_confusion_matrix = confusion_matrix(y_test, y_pred, normalize='true')
-m_confusion_matrix = np.around(m_confusion_matrix.astype('float') / m_confusion_matrix.sum(axis=1)[:, np.newaxis], decimals=2)
+y_pred = clf_svm.predict(X_test)
+svm_confusion_matrix = confusion_matrix(y_test, y_pred, normalize='true')
+svm_confusion_matrix = np.around(svm_confusion_matrix.astype('float') / svm_confusion_matrix.sum(axis=1)[:, np.newaxis], decimals=2)
+print(f"Cofusion Matrix: {svm_confusion_matrix}")
 
-y_pred = clf_svm.predict(X.numpy())
+# y_pred = clf_svm.predict(X)
 
-np.save(f"{outputs_dir}{PROJECT_NAME}_SVM.npy", y_pred)
+# np.save(f"{outputs_dir}{PROJECT_NAME}_SVM.npy", y_pred)
 
 
 # # # ----- RF -----#
 
-# clf_rf = RandomForestClassifier(
+clf_rf = RandomForestClassifier(
+    n_estimators=300,
+    min_samples_leaf=2,
+    max_depth=80,
+    min_samples_split=5,
+    bootstrap=True,
+    max_features="sqrt",
+    verbose=True
+)
 
-# n_estimators=300,
+clf_rf.fit(X_train, y_train)
+rf_accuracy = clf_rf.score(X_test, y_test)
+print(f"RF Accuracy : {rf_accuracy}")
 
-# min_samples_leaf=2,
+y_pred = clf_rf.predict(X_test)
+rf_confusion_matrix = confusion_matrix(y_test, y_pred, normalize='true')
+rf_confusion_matrix = np.around(rf_confusion_matrix.astype('float') / rf_confusion_matrix.sum(axis=1)[:, np.newaxis], decimals=2)
+print(f"Cofusion Matrix: {rf_confusion_matrix}")
 
-# max_depth=80,
-
-# min_samples_split=5,
-
-# bootstrap=True,
-
-# max_features="sqrt",
-
-# )
-
-# clf_rf.fit(X_train, y_train)
-
-# svm_accuracy = clf_rf.score(X_test.numpy(), y_test.numpy())
-# print(svm_accuracy)
-# y_pred = clf_rf.predict(X_test.numpy())
-# m_confusion_matrix = confusion_matrix(y_test, y_pred, normalize='true')
-# m_confusion_matrix = np.around(m_confusion_matrix.astype('float') / m_confusion_matrix.sum(axis=1)[:, np.newaxis], decimals=2)
-# plt.figure(dpi=500)
-# plt.matshow(m_confusion_matrix, cmap="coolwarm")
-# plt.xlabel("True Labels")
-# plt.xticks(np.arange(0,N_LABELS,1), range(1,len(labels)))
-# plt.ylabel("Predicted Labels")
-# plt.yticks(np.arange(0,N_LABELS,1), range(1,len(labels)))
-# for k in range (len(m_confusion_matrix)):
-#     for l in range(len(m_confusion_matrix[k])):
-#         # plt.text(k,l,str(m_confusion_matrix[k][l]), va='center', ha='center', fontsize='small') #trento
-#         plt.text(k,l,str(m_confusion_matrix[k][l]), va='center', ha='center', fontsize='xx-small') #houston
-# plt.savefig(f"{images_dir}{dataset}_RF_test_CONFUSION_MATRIX.png",bbox_inches='tight', pad_inches=0.2, dpi=500)
-            
-
-# y_pred = clf_rf.predict(X.numpy())
-# plt.figure(dpi=500)
-# plt.imshow(y_pred.reshape(SHAPE), interpolation='nearest', cmap = colors.ListedColormap(color[1:]))
-# plt.axis('off')
-# plt.savefig(f"{images_dir}{dataset}_RF_PREDICTIONS.png",bbox_inches='tight', pad_inches=0, dpi=500)
-
-
+# y_pred = clf_rf.predict(X_test)
 # np.save(f"{outputs_dir}{PROJECT_NAME}_RF.npy", y_pred)
+
