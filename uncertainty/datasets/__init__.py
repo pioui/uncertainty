30266d3375144b68fceece0f434dd6cc7900eeb7
pioui <piyilozou@gmail.com>
2022-06-09 01:19:05 +0200

    trento classification

--- uncertainty/datasets/__init__.py
+++ uncertainty/datasets/__init__.py
@@ -1,5 +1,7 @@
 from .bcss import bcss_Dataset
+from .trento import trento_Dataset
 
 __all__ = [
     "bcss_Dataset",
+    "trento_Dataset"
 ]
--- uncertainty/datasets/trento.py
+++ uncertainty/datasets/trento.py
@@ -1,9 +1,9 @@
-import numpy as np
+from scipy import io
+import tifffile
 from sklearn.model_selection import train_test_split
 import numpy as np
 import logging
 import random
-import imageio
 
 from uncertainty.utils import normalize
 
@@ -11,37 +11,32 @@ random.seed(42)
 # logger = logging.getLogger(__name__)
 # logger.setLevel(logging.DEBUG)
 
-label_schema = np.array(
-    [
-        0,1,2,3,4, # wanted labels
-        5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,
-    ]
-)
-class bcss_Dataset():
+class trento_Dataset():
     def __init__(
         self,
         data_dir,
-        samples_per_class=500,
+        samples_per_class = 200,
         train_size=0.5,
         do_preprocess=True,
     ) -> None:
         super().__init__()
 
-        x = np.array(imageio.imread(data_dir+"images/TCGA-AR-A1AQ-DX1_xmin18171_ymin38296_MPP-0.2500.png")) # [2260, 2545, 3]
-        y = np.array(imageio.imread(data_dir+"masks/TCGA-AR-A1AQ-DX1_xmin18171_ymin38296_MPP-0.2500.png"), dtype = np.int64)# [2260, 2545]
-        self.shape = y.shape
-
-        # x_all = np.moveaxis(x, -1, 0) # [3, 2260, 2545]
-        x_all = x.reshape(-1,x.shape[-1]) # [3, 6439719]
-        # x_all = x_all.transpose(1,0) # [6439719, 3]
-
+        image_hyper = np.array(tifffile.imread(data_dir+"hyper_Italy.tif")) # [63,166,600]
+        image_lidar = np.array(tifffile.imread(data_dir+"LiDAR_Italy.tif")) # [2,166,600]
+        x = np.concatenate((image_hyper,image_lidar), axis = 0) # [65,166,600]
+        x_all = x
+        x_all = x_all.reshape(len(x_all),-1)
+        x_all = x_all.transpose(1,0) # [99600,65]
+        
+        #Normalize to [0,1]
         if do_preprocess: 
+            # x_all = normalize(x_all).astype(float)
             x_all = normalize(x_all)
+
+        y = np.array(io.loadmat(data_dir+"TNsecSUBS_Test.mat")["TNsecSUBS_Test"], dtype = np.int64) # [166,600] 0 to 6
         y_all = y
-        print(y_all.dtype)
+        y_all = y_all.reshape(-1) # [99600]
 
-
-        y_all = label_schema[y_all.reshape(-1)] # [6439719] 0 to 5
         print(x_all.shape, y_all.shape)
         print(f"HERE{np.unique(y_all)}")
 
@@ -79,23 +74,17 @@ class bcss_Dataset():
         for l in np.unique(y_all):
             print(f'Label {l}: {np.sum(y_all==l)}')
 
+
 if __name__ == "__main__":
 
-    DATASET = bcss_Dataset(
-        data_dir = "/home/pigi/repos/BCSS/",
+    DATASET = trentoDataset(
+        data_dir = "/home/pigi/data/trento/",
     )
-
-    x,y = DATASET.full_dataset # [5731136] 0 to 20
+    x,y = DATASET.train_dataset # 819
     print(x.shape, y.shape, np.unique(y))
-    for l in np.unique(y):
-        print(f'Label {l}: {np.sum(y==l)}')
 
-    x,y = DATASET.train_dataset # [1719340] -1 to 19
-    print(x.shape, y.shape, np.unique(y)) 
-    for l in np.unique(y):
-        print(f'Label {l}: {np.sum(y==l)}')
-
-    x,y = DATASET.test_dataset # [4011796] -1 to 19
+    x,y = DATASET.test_dataset # 15107
     print(x.shape, y.shape, np.unique(y))
-    for l in np.unique(y):
-        print(f'Label {l}: {np.sum(y==l)}')
+
+    x,y = DATASET.full_dataset # 15107
+    print(x.shape, y.shape, np.unique(y))
