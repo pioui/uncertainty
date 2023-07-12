62f51921df8035b1cb5939a4c5196acbcbcdad63
pioui <piyilozou@gmail.com>
2023-07-12 16:53:48 +0300

    bcss dataset update

--- uncertainty/datasets/bcss.py
+++ uncertainty/datasets/bcss.py
@@ -1,11 +1,10 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
-import numpy as np
 import logging
 import random
 import imageio
-
-#from uncertainty.utils import normalize TODO: Why?
+from sklearn.feature_extraction import image
+from uncertainty.utils import normalize
 
 random.seed(42)
 logger = logging.getLogger(__name__)
@@ -18,46 +17,53 @@ label_schema = np.array(
 
 class bcss_dataset:
     def __init__(
-        self, data_dir, samples_per_class=500, train_size=0.001, do_preprocess=True
-    ) -> None:
+        self, data_dir, train_size=0.002, do_preprocess=True
+        ) -> None:
         super().__init__()
-
-        x = np.array(imageio.v2.imread(f"{data_dir}images/TCGA-D8-A1JG-DX1_xmin15677_ymin69205_MPP-0.2500.png"))
-        F = np.load("/work/saloua/uncertainty-main/outputs/stats_bcss.npy")[:,:,:,:6]
-        for i in range(3):
-            x = np.concatenate((x, F[i,:,:,:]), axis = -1)
-        
-        y = np.array(imageio.v2.imread(f"{data_dir}masks/TCGA-D8-A1JG-DX1_xmin15677_ymin69205_MPP-0.2500.png"), dtype=np.int64)
-        
+        print(data_dir)
+        x = np.array(
+            imageio.v2.imread(
+                f"{data_dir}BCSS/images/TCGA-D8-A1JG-DX1_xmin15677_ymin69205_MPP-0.2500.png"
+            )
+        )  # [6941, 5342, 3]
+        y = np.array(
+            imageio.v2.imread(
+                f"{data_dir}BCSS/masks/TCGA-D8-A1JG-DX1_xmin15677_ymin69205_MPP-0.2500.png"
+            ),
+            dtype=np.int64,
+        )  # [6941, 5342]
         self.shape = y.shape
 
-        x_all = x.reshape(-1, x.shape[-1])  # [6439719,3]
+        x_all = x.reshape(-1, x.shape[-1])  # [37078822, 3]
+
+        y_all = y
+        y_all = y_all.reshape(-1)  # [37078822]
+        y_all = label_schema[y_all.reshape(-1)] 
 
         if do_preprocess:
             x_all = normalize(x_all)
-        y_all = y
-        y_all = y_all.reshape(-1)  # [6439719]
-        y_all = label_schema[y_all.reshape(-1)]  # [6439719] 0 to 5
+
+        x_all = x.reshape(6941, 5342, -1)  # [6941, 5342, 3]
+
 
         self.n_classes = len(np.unique(y_all))
 
-
         x_train, x_test, y_train, y_test = train_test_split(
-            x_all[y_all!=0,:],
+            x_all[y_all!=0,:,:,:],
             y_all[y_all!=0],
             train_size=train_size,
             random_state=42,
-            stratify=y_all[y_all!=0],
+            stratify=y_all[y_all!=0],#y_all_train,
         )  
 
-        self.train_dataset = (x_train, y_train)
+        self.train_dataset = (x_train, y_train)  
         logger.info(
             f"Train dataset shape: {x_train.shape}, {y_train.shape}, {np.unique(y_train)}"
         )
         for l in np.unique(y_train):
             logger.info(f"Label {l}: {np.sum(y_train==l)}")
 
-        self.test_dataset = (x_test, y_test )
+        self.test_dataset = (x_test, y_test )  
         logger.info(
             f"Test dataset shape: {x_test.shape}, {y_test.shape}, {np.unique(y)}"
         )
@@ -70,23 +76,9 @@ class bcss_dataset:
             logger.info(f"Label {l}: {np.sum(y_all==l)}")
 
 
-def normalize(x):
-    """
-    @param x : array(channels, N)
-    @return : array(L, N) normalized x at [0,1] in channels dimention
-    """
-
-    # logger.info("Normalize to 0,1")
-    x_min = x.min(axis=0)[0]  # [57]
-    x_max = x.max(axis=0)[0]  # [57]
-    xn = (x - x_min) / (x_max - x_min)
-    assert np.unique(xn.min(axis=0)[0] == 0.0)
-    assert np.unique(xn.max(axis=0)[0] == 1.0)
-    return xn
-
 if __name__ == "__main__":
 
-    DATASET = bcss_dataset(data_dir="/work/saloua/uncertainty-main/datasets/")
+    DATASET = bcss_dataset(data_dir = "/media/data/saloua/")
 
     x, y = DATASET.full_dataset   
     print(x.shape, y.shape, np.unique(y))
