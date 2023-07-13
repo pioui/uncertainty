127f3cac8bd98a8e54f7578b0fe6426583f5183d
Saloua Chlaily <salouachlaily@gmail.com>
2023-07-13 22:31:09 +0200

    Update bcss.py
    Fix the discrepancy between predictions and data

--- uncertainty/datasets/bcss.py
+++ uncertainty/datasets/bcss.py
@@ -3,8 +3,8 @@ from sklearn.model_selection import train_test_split
 import logging
 import random
 import imageio
-from sklearn.feature_extraction import image
 from uncertainty.utils import normalize
+from sklearn.ensemble import IsolationForest
 
 random.seed(42)
 logger = logging.getLogger(__name__)
@@ -14,20 +14,40 @@ label_schema = np.array(
     [0, 1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]  # wanted labels
 )
 
+def get_high_density_data(x, y):
+    '''
+    @param x : np.array(N,D) N data points x D channels
+    @param y : np.array(N) true labels of the data points
+    @return : high density samples of the dataset
+    '''
+
+    C = np.unique(y)
+    x_new = np.array([])
+    y_new = np.array([])
+    for c in C:
+        #Get high density data points to remove outliers
+        new_labels = IsolationForest(contamination = 0.5, random_state = 42).fit_predict(x[y==c,:])
+        if x_new.size == 0:
+            x_new = x[y==c,:][new_labels==1,:]
+            y_new = y[y==c][new_labels==1]
+        else:
+            x_new = np.concatenate((x_new, x[y==c,:][new_labels==1,:]))
+            y_new = np.concatenate((y_new, y[y==c][new_labels==1]))
+    return x_new, y_new
 
 class bcss_dataset:
     def __init__(
-        self, data_dir, train_size=0.002, do_preprocess=True
+        self, data_dir, do_preprocess=True
         ) -> None:
         super().__init__()
         print(data_dir)
         x = np.array(
-            imageio.v2.imread(
+            imageio.imread(
                 f"{data_dir}BCSS/images/TCGA-D8-A1JG-DX1_xmin15677_ymin69205_MPP-0.2500.png"
             )
         )  # [6941, 5342, 3]
         y = np.array(
-            imageio.v2.imread(
+            imageio.imread(
                 f"{data_dir}BCSS/masks/TCGA-D8-A1JG-DX1_xmin15677_ymin69205_MPP-0.2500.png"
             ),
             dtype=np.int64,
@@ -43,17 +63,26 @@ class bcss_dataset:
         if do_preprocess:
             x_all = normalize(x_all)
 
-        # x_all = x.reshape(6941, 5342, -1)  # [6941, 5342, 3]
+        x_train, x_test, y_train, y_test = train_test_split(
+                x_all[y_all!=0,:],
+                y_all[y_all!=0],
+                train_size=0.02,
+                random_state=42,
+            )
+
+        x_test, _, y_test, _ = train_test_split(
+                x_test, 
+                y_test,
+                train_size=0.02,
+                random_state=42,
+            )
+
+        # Get high density data
+        x_train, y_train = get_high_density_data(x_train, y_train)
+        x_test, y_test = get_high_density_data(x_test, y_test)
 
         self.n_classes = len(np.unique(y_all))
 
-        x_train, x_test, y_train, y_test = train_test_split(
-            x_all[y_all!=0,:],
-            y_all[y_all!=0],
-            train_size=train_size,
-            random_state=42,
-            stratify=y_all[y_all!=0],#y_all_train,
-        )  
 
         self.train_dataset = (x_train, y_train)  
         logger.info(
